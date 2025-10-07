bbe3f70fb17c796f03d840f08a2c0537e7729a68
htung0403 <htung0403@gmail.com>
2025-10-08 03:46:40 +0700

    Add Todo and User entity classes with properties and validation attributes

---
+++ server/Models/Entities/Todo.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models.Entities
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [MaxLength(1000)]
+        public string? Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public DateTime? CompletedAt { get; set; }
+
+        // Foreign Key
+        [Required]
+        public int UserId { get; set; }
+
+        // Navigation property
+        public User User { get; set; } = null!;
+    }
+}
---
+++ server/Models/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(255)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        // Navigation property
+        public ICollection<Todo> Todos { get; set; } = new List<Todo>();
+    }
+}
