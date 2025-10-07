d499423e02de4107f1c8b0f06b8d301f2887e84e
Your Name <htung0403@gmail.com>
2025-10-08 00:07:52 +0700

    Add project files.

---
+++ server/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Entities.TodoItem> TodoItems => Set<Entities.TodoItem>();
+    }
+}
